1aa4e821bcb95b6c568fa259bf97651af82955cf
rima <rtoute311@gmail.com>
2024-09-19 11:19:41 +0100

    first commit

---
+++ src/main/java/tn/esprit/springprojectesprit/SpringProjectEspritApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.springprojectesprit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectEspritApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProjectEspritApplication.class, args);
+    }
+
+}
